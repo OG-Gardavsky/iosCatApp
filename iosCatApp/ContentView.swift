91e7265217acbb3976c0673f341e24bc43e7ebd6
OG-Gardavsky <ogardavsky@gmail.com>
2022-06-06 09:07:07 +0200

    Initial Commit

---
+++ iosCatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosCatApp
+//
+//  Created by  Ondrej Gardavsky on 06.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iosCatApp/iosCatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosCatAppApp.swift
+//  iosCatApp
+//
+//  Created by  Ondrej Gardavsky on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct iosCatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
