0417fb9442259bd31f6a46c438ef605c8ee81f03
OG-Gardavsky <ogardavsky@gmail.com>
2022-06-06 12:33:43 +0200

    nejaky base zmeny

---
+++ iosCatApp/Scenes/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  iosCatApp
+//
+//  Created by  Ondrej Gardavsky on 06.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView  {
+            
+            
+            
+            NavigationView {
+                BreedList()
+            }
+            
+           
+            
+            
+            
+            
+            
+            
+            
+        }
+    }
+}
